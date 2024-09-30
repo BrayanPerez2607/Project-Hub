3b6eb2d8ccfced17616cc2c58288a5246220af6a
BrayanPerez2607 <brayperez2607@gmail.com>
2024-09-29 23:52:04 -0500

    second commit

---
+++ project-hub/server/middleware/middleware.go
@@ -0,0 +1,76 @@
+package middleware
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+var collection *mongo.Collection
+
+func init() {
+	loadTheEnv()
+	CreateDBInstance()
+}
+
+func loadTheEnv() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal("Error loading the .env file")
+	}
+}
+
+func CreateDBInstance() {
+	connectionString := os.Getenv("DB_URI")
+	dbName := os.Getenv("DB_NAME")
+	collName := os.Getenv("DB_COLLECTION_NAME")
+
+	clientOptions := options.Client().ApplyURL(connectionString)
+
+	client, err := mongo.connect(context.TODO(), clientOptions)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = client.Ping(context.TODO(), nil)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("connected to mongodb!")
+
+	collection = client.Database(dbName).Collection(collName)
+	fmt.Println("collection instance created!")
+}
+
+func GetAllTasks(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func CreateTask() {
+
+}
+
+func TaskComplete() {
+
+}
+
+func UndoTask() {
+
+}
+
+func DeleteTask() {
+
+}
+
+func DeleteAllTasks() {
+
+}
